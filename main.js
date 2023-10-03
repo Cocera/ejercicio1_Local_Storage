a98e7d5aee72ce3bcadd480a280a08997daefc91
Cocera <alcocera@gmail.com>
2023-10-03 16:48:19 +0200

    Extra

--- main.js
+++ main.js
@@ -6,19 +6,43 @@ const userInfoP = document.getElementById("userInfoP")
 
 
 
-function saveInfo(e) {
+// function saveInfo(e) {
+//     e.preventDefault();
+//     const user1 = {name: infoName.value, mail: infoMail.value, message: infoMessage.value};
+    
+//     // pasar a local storage
+//     localStorage.setItem("user1", JSON.stringify(user1));
+    
+//     // registrar al usuario en el HTML
+//     const infoUser = JSON.parse(localStorage.getItem("user1"));
+//     userInfoP.innerHTML = `Tu nombre es ${infoUser.name}, tu mail ${infoUser.mail} y tu comentario: ${infoUser.message}`;
+// };
+
+// btn.addEventListener("click", saveInfo);
+
+const arrayUsers = []
+
+function saveUsers(e) {
     e.preventDefault();
-    const user1 = {name: infoName.value, mail: infoMail.value, message: infoMessage.value};
-    
-    // pasar a local storage
-    localStorage.setItem("user1", JSON.stringify(user1));
-    
-    // registrar al usuario en el HTML
-    const infoUser = JSON.parse(localStorage.getItem("user1"));
-    // OPTION concatenar
-    // userInfoP.innerHTML = "Nombre: " + infoUser.name + "\nMail: " + infoUser.mail + "\nMensaje:" + infoUser.message;
-    userInfoP.innerHTML = `Tu nombre es ${infoUser.name}, tu mail ${infoUser.mail} y tu comentario: ${infoUser.message}`;
+    userInfoP.innerHTML = "";
+    const userRandom = {name: infoName.value, mail: infoMail.value, message: infoMessage.value};
+    arrayUsers.push(userRandom);
+    document.getElementById("myForm").reset();
+    localStorage.setItem("arrayUsers", JSON.stringify(arrayUsers));
+
+    let dataUsers = JSON.parse(localStorage.getItem("arrayUsers"));
+
+    for (let i=0; i<dataUsers.length; i++) {
+        // let newP = document.createElement("p");
+        // document.body.appendChild(newP);
+        userInfoP.innerHTML += `<p> Tu nombre es ${dataUsers[i].name}, tu mail ${dataUsers[i].mail} y tu comentario: ${dataUsers[i].message} </p>`
+    }
+
+   
 };
 
 
-btn.addEventListener("click", saveInfo);
+
+
+
+btn.addEventListener("click", saveUsers);
