0507fd4e2c60a73680b448d27fa285528897c212
Cocera <alcocera@gmail.com>
2023-10-03 13:21:07 +0200

    Info in DOM

--- main.js
+++ main.js
@@ -9,11 +9,16 @@ const userInfoP = document.getElementById("userInfoP")
 function saveInfo(e) {
     e.preventDefault();
     const user1 = {name: infoName.value, mail: infoMail.value, message: infoMessage.value};
+    
     // pasar a local storage
     localStorage.setItem("user1", JSON.stringify(user1));
+    
     // registrar al usuario en el HTML
-    const infoUser = JSON.parse(localStorage.getItem(user1));
-    console.log(infoUser)
+    const infoUser = JSON.parse(localStorage.getItem("user1"));
+    // OPTION concatenar
+    // userInfoP.innerHTML = "Nombre: " + infoUser.name + "\nMail: " + infoUser.mail + "\nMensaje:" + infoUser.message;
+    userInfoP.innerHTML = `Tu nombre es ${infoUser.name}, tu mail ${infoUser.mail} y tu comentario: ${infoUser.message}`;
 };
+
 
 btn.addEventListener("click", saveInfo);
