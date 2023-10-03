423b0d077223582fa09d36de99f31cf1bf744e89
Cocera <alcocera@gmail.com>
2023-10-03 17:03:15 +0200

    Delete local storage

--- main.js
+++ main.js
@@ -2,23 +2,23 @@ const infoMail = document.getElementById("mail");
 const infoName = document.getElementById("name");
 const infoMessage = document.getElementById("message");
 const btn = document.getElementById("btn");
-const userInfoP = document.getElementById("userInfoP")
+const userInfoP = document.getElementById("userInfoP");
+const btn_delete = document.getElementById("btn_delete")
 
 
-
+// PRIMERA FASE
 // function saveInfo(e) {
 //     e.preventDefault();
-//     const user1 = {name: infoName.value, mail: infoMail.value, message: infoMessage.value};
-    
-//     // pasar a local storage
-//     localStorage.setItem("user1", JSON.stringify(user1));
-    
-//     // registrar al usuario en el HTML
+//     const user1 = {name: infoName.value, mail: infoMail.value, message: infoMessage.value};  
+//     localStorage.setItem("user1", JSON.stringify(user1));    
 //     const infoUser = JSON.parse(localStorage.getItem("user1"));
 //     userInfoP.innerHTML = `Tu nombre es ${infoUser.name}, tu mail ${infoUser.mail} y tu comentario: ${infoUser.message}`;
 // };
-
 // btn.addEventListener("click", saveInfo);
+
+
+
+// EXTRA
 
 const arrayUsers = []
 
@@ -33,16 +33,19 @@ function saveUsers(e) {
     let dataUsers = JSON.parse(localStorage.getItem("arrayUsers"));
 
     for (let i=0; i<dataUsers.length; i++) {
-        // let newP = document.createElement("p");
-        // document.body.appendChild(newP);
-        userInfoP.innerHTML += `<p> Tu nombre es ${dataUsers[i].name}, tu mail ${dataUsers[i].mail} y tu comentario: ${dataUsers[i].message} </p>`
+        userInfoP.innerHTML += `<p>Tu nombre es ${dataUsers[i].name}, tu mail ${dataUsers[i].mail} y tu comentario: ${dataUsers[i].message}</p>`
     }
 
-   
 };
 
-
-
-
-
 btn.addEventListener("click", saveUsers);
+
+function deleteUsers() {
+    localStorage.clear();
+};
+
+btn_delete.addEventListener("click", deleteUsers);
+
+
+
+
