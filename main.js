64fe8622622ab5c799b9afebceaef7104205c982
Cocera <alcocera@gmail.com>
2023-10-03 12:10:12 +0200

    HTML elements to JS

---
+++ main.js
@@ -0,0 +1,6 @@
+const infoMail = document.getElementById("mail");
+const infoName = document.getElementById("name");
+const infoMessage = document.getElementById("message");
+const btn = document.getElementById("btn");
+
+
