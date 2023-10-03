63a06b849f3954cbe02d9808ba3a922d751867f5
Cocera <alcocera@gmail.com>
2023-10-03 12:13:46 +0200

    JS linked & function

--- main.js
+++ main.js
@@ -3,4 +3,11 @@ const infoName = document.getElementById("name");
 const infoMessage = document.getElementById("message");
 const btn = document.getElementById("btn");
 
+console.log(infoMail)
+console.log(infoName)
+console.log(infoMessage)
+console.log(btn)
 
+function saveInfo() {
+    console.log('Hey');
+}
